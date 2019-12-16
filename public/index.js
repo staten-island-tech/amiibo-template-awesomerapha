6a886fb3dd787c5628801e18a2cf393bd3812ffb
awesomerapha <raphael.morgenshtern@gmail.com>
2019-12-16 11:43:46 -0500

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express"); 
+const app = express();
+
+app.get("", (req, res) => {
+    res.send("Thami likes bagels and butter. Also he smells");
+});
+app.get("/thamisucks", (req, res) => {
+    res.send("Thami sucks");
+});
+app.listen(3000, ()=> {
+    console.log('Listening on port 3000');
+});
+//127.0.0.1:3000
