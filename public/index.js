255f93b58ca676395aec26188f1629f00da2186c
awesomerapha <raphael.morgenshtern@gmail.com>
2019-12-17 11:45:05 -0500

    h

--- public/index.js
+++ public/index.js
@@ -2,8 +2,19 @@ const path = require("path");
 const express = require("express"); 
 const app = express();
 
-app.get("", (req, res) => {
-    res.send("Thami likes bagels and butter. Also he smells");
+//define paths for express
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+//setup handlebars engine and views location
+app.set("views engine", "hbs"); //Telling express/NODE to use handlebars for templates
+app.set("views", viewsPath);//telling express to get templates from templates/views folder
+app.get("", async (req, res) => {
+    try {
+        res.render("index");
+    } catch(error){
+        res.status(500).send("Page Not Found");
+        
+    }
 });
 app.get("/thamisucks", (req, res) => {
     res.send("Thami sucks");
@@ -12,3 +23,4 @@ app.listen(3000, ()=> {
     console.log('Listening on port 3000');
 });
 //127.0.0.1:3000
+//
