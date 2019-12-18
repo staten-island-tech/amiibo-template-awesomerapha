a588327a1e9ea95f5e4d66a3725558ce83f1047a
awesomerapha <raphael.morgenshtern@gmail.com>
2019-12-18 11:41:38 -0500

    a

--- public/index.js
+++ public/index.js
@@ -1,16 +1,31 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express"); 
 const app = express();
 
 //define paths for express
 const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials")
 const viewsPath = path.join(__dirname, "../templates/views");
 //setup handlebars engine and views location
-app.set("views engine", "hbs"); //Telling express/NODE to use handlebars for templates
+app.set("view engine", "hbs"); //Telling express/NODE to use handlebars for templates
 app.set("views", viewsPath);//telling express to get templates from templates/views folder
-app.get("", async (req, res) => {
+hbs.registerPartials(partialsPath);
+app.get("/about/:id", async (req, res) => {
+    const name = req.params.id;
     try {
-        res.render("index");
+        res.render("index",  {
+            title: name
+        });
+    } catch(error){
+        res.status(500).send("Page Not Found");
+        
+    }
+});
+app.get("/swag", async (req, res) => {
+    const name = req.params.id;
+    try {
+        res.render("swag");
     } catch(error){
         res.status(500).send("Page Not Found");
         
